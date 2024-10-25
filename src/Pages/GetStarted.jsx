52c2a96192d3bc9b0b72f4ab2f673d418be8fa44
Lekan <abdulmalikawesu@gmail.com>
2024-10-25 21:44:21 +0100

    progress

--- src/Pages/GetStarted.jsx
+++ src/Pages/GetStarted.jsx
@@ -9,7 +9,7 @@ const GetStarted = () => {
         <div>
             <Navbar />
 
-            <div className="w-full min-h-screen flex flex-col gap-10 items-center justify-center py-10" style={{
+            <div className="w-full min-h-screen flex flex-col gap-10 items-center justify-center px-5 py-10" style={{
                 backgroundImage: `url(${start})`, // Correctly set background image
                 backgroundSize: 'cover', // Optional: cover to ensure the image fills the div
             }}>
