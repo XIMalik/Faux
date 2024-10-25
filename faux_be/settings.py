b0e0db28c842b8a9689ded698ecf91498a455706
Lekan <abdulmalikawesu@gmail.com>
2024-10-25 15:27:42 +0100

    update

--- faux_be/settings.py
+++ faux_be/settings.py
@@ -91,7 +91,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "faux.onrender.com",
+    "https://faux.onrender.com",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
